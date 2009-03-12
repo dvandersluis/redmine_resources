d7b3b6e864fcd033d37c3b87a59470bbbe03ca2d
dvandersluis <daniel@codexed.com>
2009-03-12 17:13:41 +0000

    Added ViewListener extension to add :if to render_on; updated render_on call to use :if instead of checking within the view.

--- init.rb
+++ init.rb
@@ -7,7 +7,7 @@ Redmine::Plugin.register :redmine_resources do
   name 'Issue Resources'
   author 'Daniel Vandersluis'
   description 'Allows links to resources to be attached to an issue'
-  version '0.1.0'
+  version '0.1.2'
 
   project_module :resources do |map|
     map.permission :view_resources, { }
--- lib/resources_show_issue_hook.rb
+++ lib/resources_show_issue_hook.rb
@@ -1,8 +1,39 @@
+# Extension to ViewListener to allow an :if parameter to render_on.
+# See <http://www.redmine.org/boards/3/topics/show/4316>
+module Redmine
+  module Hook
+    class ViewListener
+      def self.render_on(hook, options={})
+        define_method hook do |context|
+          if !options.include?(:if) || evaluate_if_option(options[:if], context)
+            context[:controller].send(:render_to_string, {:locals => context}.merge(options))
+          end
+        end
+      end
+
+      private
+
+      def evaluate_if_option(if_option, context)
+        case if_option
+        when Symbol
+          send(if_option, context)
+        when Method, Proc
+          if_option.call(context)
+        end        
+      end
+    end
+  end
+end
+
 class ShowIssueResourcesHook < Redmine::Hook::ViewListener
-  render_on :view_issues_show_desc_bottom, :partial => "issues/resources" 
+  render_on :view_issues_show_desc_bottom, :partial => "issues/resources", :if => :has_permission? 
 
 private
   def protect_against_forgery?
     false
+  end
+
+  def has_permission?(context)
+    context[:project].module_enabled?('resources') and User.current.allowed_to?(:view_resources, context[:project])
   end
 end
