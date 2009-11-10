8ea9d63f08c800299f136616c7f4644dc36f572b
pdf <github@obfusc8.org>
2009-11-10 14:30:36 +1100

    Move IssuePatch into RedmineResources module to avoid conflicts with other plugins

--- init.rb
+++ init.rb
@@ -1,6 +1,17 @@
 require 'redmine'
 
-require_dependency 'resources_issue_patch'
+# Patches to the Redmine core.
+require 'dispatcher'
+
+Dispatcher.to_prepare :redmine_resources do
+  require_dependency 'issue'
+  # Guards against including the module multiple time (like in tests)
+  # and registering multiple callbacks
+  unless Issue.included_modules.include? RedmineResources::IssuePatch
+    Issue.send(:include, RedmineResources::IssuePatch)
+  end
+end
+
 require_dependency 'resources_show_issue_hook'
 
 Redmine::Plugin.register :redmine_resources do
---
+++ lib/redmine_resources/issue_patch.rb
@@ -0,0 +1,16 @@
+module RedmineResources
+  # Patches Redmine's Issues dynamically. Adds a relationship
+  # Issue +has_many+ to IssueResource
+  module IssuePatch
+    def self.included(base) # :nodoc:
+      # Same as typing in the class
+      base.class_eval do
+        belongs_to :deliverable
+        has_many :resources, :class_name => 'IssueResource', :foreign_key => 'issue_id', :dependent => :delete_all
+      end
+   
+    end
+  end
+  
+  Issue.send(:include, IssuePatch)
+end
